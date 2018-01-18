1b496eaff2958ab4602a6f4fec5d737c5629506a
Tom Dyson <tom@torchbox.com>
2018-01-18 20:09:14 +0000

    Sightings model

--- lylesightings/core/models.py
+++ lylesightings/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from location_field.models.plain import PlainLocationField
 
-# Create your models here.
+
+class Sighting(models.Model):
+    sighter = models.CharField(max_length=200, help_text="Who saw the Lyle?")
+    description = models.TextField()
+    datetime = models.DateTimeField('date and time seen')
+    url = models.CharField(max_length=250, verbose_name="URL")
+    city = models.CharField(max_length=255, null=True, blank=True)
+    location = PlainLocationField(based_fields=['city'], zoom=7)
+
+    def __str__(self):
+        return '{sighter} in {city}'.format(sighter=self.sighter, city=self.city)
