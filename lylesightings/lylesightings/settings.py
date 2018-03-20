2b7ca69f4c66d17e77431788d1fcc36c9c605e6a
Tom Dyson <tom@torchbox.com>
2018-03-20 09:32:32 +0000

    static root

--- lylesightings/lylesightings/settings.py
+++ lylesightings/lylesightings/settings.py
@@ -114,10 +114,11 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
