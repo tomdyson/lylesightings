a3a881ac574e951bb005c4c824cf0cf0cf939e19
Tom Dyson <tom@torchbox.com>
2018-03-23 00:05:33 +0000

    add image to sightings model

--- lylesightings/core/models.py
+++ lylesightings/core/models.py
@@ -6,6 +6,7 @@ class Sighting(models.Model):
     sighter = models.CharField(max_length=200, help_text="Who saw the Lyle?")
     description = models.TextField()
     datetime = models.DateTimeField('date and time seen')
+    photo = models.ImageField(upload_to='sighting-photos', null=True)
     url = models.CharField(max_length=250, verbose_name="URL", unique=True)
     city = models.CharField(max_length=255, null=True, blank=True)
     location = PlainLocationField(based_fields=['city'], zoom=7, null=True, blank=True)
