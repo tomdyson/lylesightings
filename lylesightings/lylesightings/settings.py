fdb6e334dd19482359ccc693b3d03e36dc0e6e7e
Tom Dyson <tom@torchbox.com>
2018-03-25 14:19:32 +0100

    Use imported media, not Instagram embeds

--- lylesightings/lylesightings/settings.py
+++ lylesightings/lylesightings/settings.py
@@ -123,3 +123,4 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- lylesightings/lylesightings/urls.py
+++ lylesightings/lylesightings/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.conf.urls import url
 from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from core import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    url(r'^admin/', admin.site.urls),
-]
+urlpatterns = [path('', views.index, name='index'), url(r'^admin/', admin.site.urls)]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
